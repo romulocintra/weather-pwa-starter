1ab3d4eaf1ad9b8fc98e2debacc0e1318581b02e
Romulo Cintra <romulocintra@gmail.com>
2021-04-29 14:00:30 +0200

    step 1

---
+++ app.js
@@ -0,0 +1,14 @@
+const apiKey = 'c824b687505862ac13832632bdde7b0f';
+const form = document.querySelector('#weather-form');
+const placeholder = document.querySelector('#weather-result')
+const input = document.querySelector("input");
+// const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+// img url https://openweathermap.org/img/wn/${data.weather[0].icon}@4x.png
+
+function getTheWeather(city){
+}
+
+
+form.addEventListener("submit", async e => {
+    e.preventDefault();
+})
